b77ff4e542c532e9c0276c93163d59eca41f0723
Steve Riley <15844719+SteveJR86@users.noreply.github.com>
2025-10-19 16:43:40 +0100

    updated style and script indent to 4 from 2 spaces

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const navBtn = document.getElementById('navBtn');
 navBtn.addEventListener('click', openclosemenu);
 menu.addEventListener('click', openclosemenu);
 
-function openclosemenu(){
-  if(menu.classList.contains('menuShow')){
-    menu.classList.remove('menuShow');
-    menu.classList.add('menuHide');
-    menuIcon.src = "./assets/icons/bars-solid.svg";
-    navBtn.ariaLabel = "Hide menu";
-  } else {
-    menu.classList.remove('menuHide');
-    menu.classList.add('menuShow');
-    menuIcon.src = "./assets/icons/xmark-solid.svg";
-    navBtn.ariaLabel = "Show menu";
-  }
+function openclosemenu() {
+    if (menu.classList.contains('menuShow')) {
+        menu.classList.remove('menuShow');
+        menu.classList.add('menuHide');
+        menuIcon.src = "./assets/icons/bars-solid.svg";
+        navBtn.ariaLabel = "Hide menu";
+    } else {
+        menu.classList.remove('menuHide');
+        menu.classList.add('menuShow');
+        menuIcon.src = "./assets/icons/xmark-solid.svg";
+        navBtn.ariaLabel = "Show menu";
+    }
 }
